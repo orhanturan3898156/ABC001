cc86ec83765f12445d6b1730cbe8bdaa032de15e
orhanturan3898156 <64173402+orhanturan3898156@users.noreply.github.com>
2020-07-27 21:53:51 -0400

    heyo

--- src/test/java/Test/Test3.java
+++ src/test/java/Test/Test3.java
@@ -3,6 +3,8 @@ package Test;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("jkskla");
+        System.out.println("heyo");
+        
       
     }
 }
