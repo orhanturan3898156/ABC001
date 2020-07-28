9c8538f1f66b5c7fcd763bea645104c06deee284
orhanturan3898156 <orhanturan1566@gmail.com>
2020-07-27 21:19:26 -0400

    commit

--- src/test/java/Test/Test3.java
+++ src/test/java/Test/Test3.java
@@ -3,10 +3,7 @@ package Test;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("jkskla");
-<<<<<<< HEAD
-        System.out.println("heyo");
-=======
        System.out.println("heyhey");
->>>>>>> origin/master
+
     }
 }
