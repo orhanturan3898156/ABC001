f2aad3017996c4bff5fe2049b9f7ede54c8c64e9
orhanturan3898156 <64173402+orhanturan3898156@users.noreply.github.com>
2020-08-03 20:18:42 -0400

    Update Test3.java

--- src/test/java/Test/Test3.java
+++ src/test/java/Test/Test3.java
@@ -3,6 +3,7 @@ package Test;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("jkskla");
+        System.out.println("jkskla");
        
         
       
