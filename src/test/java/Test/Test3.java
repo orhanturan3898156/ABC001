b53e52a0fab36cd9aa2081489fc4d86fad77bba2
orhanturan3898156 <orhanturan1566@gmail.com>
2020-08-03 20:31:08 -0400

    commit added

--- src/test/java/Test/Test3.java
+++ src/test/java/Test/Test3.java
@@ -3,10 +3,7 @@ package Test;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("jkskla");
-        System.out.println("jkskla");
-        System.out.println("1oo");
-        System.out.println("2oo");
-        System.out.println("3oo");
+
 
         
       
