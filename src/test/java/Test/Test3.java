02d8fa2f5de946b3c212c03e199caf4b4c007d22
orhanturan3898156 <orhanturan1566@gmail.com>
2020-08-03 20:43:04 -0400

    commit

--- src/test/java/Test/Test3.java
+++ src/test/java/Test/Test3.java
@@ -7,8 +7,7 @@ public class Test3 {
         System.out.println(200);
         System.out.println(300);
         System.out.println(400);
-
-        System.out.println(500);
+        System.out.println(600);
 
 
         
