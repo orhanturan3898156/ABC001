cb021394c21032c3eda2323d1ea82b6316b36851
orhanturan3898156 <orhanturan1566@gmail.com>
2020-08-03 20:23:53 -0400

    commit added

--- src/test/java/Test/Test3.java
+++ src/test/java/Test/Test3.java
@@ -4,6 +4,7 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("jkskla");
         System.out.println("jkskla");
+        System.out.println("1oo");
 
         
       
