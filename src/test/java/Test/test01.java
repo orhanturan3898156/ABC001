af31808f9f600d7449c23448c811276061d82809
orhanturan3898156 <64173402+orhanturan3898156@users.noreply.github.com>
2020-08-06 07:39:32 -0400

    Update test01.java

--- src/test/java/Test/test01.java
+++ src/test/java/Test/test01.java
@@ -4,14 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("hi");
 
-        System.out.println("first project");
-        System.out.println("second project");
-        System.out.println("second project");
-        System.out.println(300);
-        System.out.println(300);
-
-        ///hiii
-
 
 
     }
